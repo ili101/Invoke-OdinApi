e788ccc440401305035a6a81b48deaad0b28913c
ili101 <zoby101@gmail.com>
2016-09-06 18:30:01 +0300

    Restructure files tree

--- Invoke-OdinApi-master.psm1
+++ Invoke-OdinApi-master.psm1
@@ -1,2 +1,5 @@
-﻿#Get-ChildItem -Path $PSScriptRoot | Unblock-File
+﻿Get-ChildItem -Path $PSScriptRoot | Unblock-File
 Get-ChildItem -Path $PSScriptRoot\*.ps1 | Foreach-Object{ . $_.FullName }
+
+Export-ModuleMember -Function Format-Xml
+Export-ModuleMember -Function Invoke-OdinApi
